5ce6e44fa3f488722c3049fe799487cd2f5caeae
SHRIKAVIN B <145219889+SHRIKAVIN@users.noreply.github.com>
2025-06-04 09:56:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //TMDB 
 
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
-const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '8dc674e';
+const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=8dc674e';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
